425ea8292863cbd8730af49df9957cb2d310cdd8
RainbowFloyd <rainbowfloyd911@gmail.com>
2018-08-10 02:00:00 -0700

    calCount component rendering

--- client/components/app.js
+++ client/components/app.js
@@ -1,5 +1,10 @@
 angular.module('app')
+.controller('appCtrl', function($scope) {
+	this.calCount = 0
+})
+
 .component('app', {
 	bindings: {},
+	controller: 'appCtrl',
 	templateUrl: '../templates/app.html'
-});
+})
---
+++ client/components/calCount.js
@@ -0,0 +1,7 @@
+angular.module('app')
+.component('calCount', {
+	bindings: {
+		calCount: '<'
+	},
+	templateUrl: '../templates/calCount.html'
+});
